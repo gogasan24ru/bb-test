1ea1ff5121085d7399f7905fbb163c1b61b94f35
gogasan24ru <a@gogasan.tk>
2018-10-18 17:28:56 +0300

    Добавьте файлы проекта.

---
+++ backend2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace backend2
+{
+    class Program
+    {
+        /// <summary>
+        /// Generate demo database with hardcoded random values
+        /// </summary>
+        static void DemoInit()
+        {
+            using (var ctx = new Model1())
+            {
+                List<String> Names = new List<string>() { "Михаил", "Виталий","Петр","Антон","Вадим","Валерий" };
+                List<String> Surnames = new List<string>() { "Иванов", "Петров","Сидоров","Лукьяненко" };
+                List<String> Passwords = new List<string>() { "cjhjrnsc", "zxj6tpmz","yd40gece","yekb9fyf" };//y
+                //List<String> Logins = new List<string>() { "user1", "" };//autoincrement in demo code
+                DateTime start = DateTime.Now;
+
+                //ctx.init();
+                var rnd=new Random();//4
+                var generated=new List<User>();
+                for (int i = 1; i < 31; i++)
+                {
+                    generated.Add(
+                        new User()
+                        {
+                            Age= rnd.Next(18,100),
+                            Login = "user"+i,
+                            Name=Names[rnd.Next(0,Names.Count)],
+                            Passport = new Passport() { Number = rnd.Next(0, 9999), Serial = rnd.Next(0, 999999) },
+                            Password = Passwords[rnd.Next(0, Passwords.Count)],
+                            Sex = sex.Male,//equal 0
+                            Surname = Surnames[rnd.Next(0, Surnames.Count)]
+                        }
+                        );
+                }
+
+
+                ctx.Users.AddRange(generated);
+                ctx.SaveChanges();
+
+                Console.WriteLine("New demo database created in (millis) " +
+                                  (DateTime.Now - start).TotalMilliseconds);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+//            Console.WriteLine(args[0]);
+//            Console.ReadKey();
+            if (args.Length >= 1)
+            {
+                if (args[0] == "--help")
+                {
+                    throw new Exception("No help supplied yet :)");
+                }
+
+                if (args[0] == "--create-with-demo")
+                    DemoInit();
+                if (args[0] == "--drop-database")
+                    Console.WriteLine((new Model1()).Database.Delete() ? "Successfully done" : "Done with error (no database?)");
+
+                if (args[0] == "--create-database")
+                {
+                    DateTime start = DateTime.Now;
+                    Console.WriteLine((new Model1()).Database.CreateIfNotExists()
+                        ? "New database created in (millis) " + (DateTime.Now - start).TotalMilliseconds.ToString()
+                        : "Already exist");
+
+                }
+
+            }
+        }
+    }
+}
---
+++ backend2/User.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public enum sex
+    {
+        Male = 0 , Female = 1
+    }
+
+    public class Passport
+    {
+
+        [Index]
+        public int PassportID { get; set; }
+
+        public int Serial { get; set; }
+
+        public int Number { get; set; }
+
+        public Passport(int serial, int number)
+        {
+            this.Serial = serial;
+            this.Number = number;
+        }
+        public Passport()
+        {
+        }
+    }
+
+    public class User
+    {
+
+        [Index]
+//        [Index(false, true, 0)]
+        public int UserID { get; set; }
+        public sex Sex { get; set; }
+        public int Age { get; set; }//ushort?
+
+        [MaxLength(8), MinLength(1)]
+        [Index(IsClustered=false,IsUnique = true,Order=0)]//Should be
+        public string Login { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [MaxLength(30)]
+        public string Surname { get; set; }
+
+//        [MaxLength(8), MinLength(4)]
+        public string Password { get; set; }
+        public Passport Passport { get; set; }
+
+//        public User(sex sex, ushort age, string login, string name, string surename, string password, Passport passport)
+//        {
+//            Sex = sex;
+//            Age = age;
+//            Login = login ?? throw new ArgumentNullException(nameof(login));
+//            Name = name ?? throw new ArgumentNullException(nameof(name));
+//            Surname = surename ?? throw new ArgumentNullException(nameof(surename));
+//            Password = password ?? throw new ArgumentNullException(nameof(password));
+//            Passport = passport ?? throw new ArgumentNullException(nameof(passport));
+//        }
+//        public User(sex sex, ushort age, string login, string name, string surename, string password, ushort serial, UInt32 number)
+//        {
+//            Sex = sex;
+//            Age = age;
+//            Login = login ?? throw new ArgumentNullException(nameof(login));
+//            Name = name ?? throw new ArgumentNullException(nameof(name));
+//            Surname = surename ?? throw new ArgumentNullException(nameof(surename));
+//            Password = password ?? throw new ArgumentNullException(nameof(password));
+//            Passport = new Passport(serial, number);
+//        }
+    }
+
+
+    //[DbConfigurationType(typeof(MySqlEFConfiguration))]
+//    public class DBusers : DbContext
+//    {
+//        public DBusers() : base()
+//        {
+//
+//        }
+//
+//        public DbSet<User> Users { get; set; }
+//        public DbSet<Passport> Passports { get; set; }
+//
+//
+//    }
+}
