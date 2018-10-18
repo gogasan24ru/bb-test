763b0822c272fc4d67d7908939dac16b69bad44d
gogasan24ru <a@gogasan.tk>
2018-10-18 17:41:23 +0300

    add empty wcf stuff

---
+++ backend2/wcf-stuff/ServerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    //    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    //    111
+    [ServiceBehavior]
+    public class ServerService : IServerService
+    {
+        private string _sessionName;
+
+        public ServerService()
+        {
+            _sessionName = "upstart";
+        }
+
+        public void StoreSession(string sessionName)
+        {
+            _sessionName = sessionName;
+        }
+
+        public string GetSessionName()
+        {
+            return _sessionName;
+        }
+
+        //public return MethodName(args?)
+        //{
+        //    smthing
+        //}
+
+    }
+}
