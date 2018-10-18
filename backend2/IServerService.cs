bae00b8a3aa56910de0f458a4182dfeb73626324
gogasan24ru <a@gogasan.tk>
2018-10-18 17:52:46 +0300

    add System.ServiceModel

---
+++ backend2/IServerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    [ServiceContract]
+//    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples", SessionMode = SessionMode.Required)]
+
+    interface IServerService
+    {
+        //[OperationContract]
+        //return MethodName(args?);
+		//..
+
+    }
+}
