c548ee5ba909ef2c64efbbc0609cde1b3f69989b
gogasan24ru <a@gogasan.tk>
2018-10-18 18:58:38 +0300

    some method incapsulating, add dummy events system

--- backend2/Program.cs
+++ backend2/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using Server;
@@ -11,10 +14,36 @@ namespace backend2
     
     class Program
     {
+        public static List<Event> events;
+
+        public static void Log(string msg)
+        {
+            events.Add(new Event(msg));
+        }
+
+        /// <summary>
+        /// Start service
+        /// </summary>
+        /// <returns>Service entity. You should stop it, when shutting down app</returns>
+        private static ServiceHost  RunService()
+        {
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            //            smb.HttpGetBinding=new BasicHttpBinding();
+            ServiceHost host = new ServiceHost(smb, new Uri("http://localhost:8000/ServerService"));
+            host.Open();
+//            Console.WriteLine("The service is ready.");
+//            Console.WriteLine("Press <ENTER> to terminate service.");
+//            Console.WriteLine();
+//            Console.ReadLine();
+//            host.Close();
+            return host;
+        }
+
         /// <summary>
         /// Generate demo database with hardcoded random values
         /// </summary>
-        static void DemoInit()
+        private static void DemoInit()
         {
             using (var ctx = new Model1())
             {
@@ -57,7 +86,11 @@ namespace backend2
         {
 //            Console.WriteLine(args[0]);
 //            Console.ReadKey();
-            if (args.Length >= 1)
+            ServiceHost host = null;
+            events = new List<Event>();
+            events.Add(new Event("Application start."));
+
+            if (args.Length >= 1)//TODO: replace with advanced args parsing!
             {
                 if (args[0] == "--help")
                 {
@@ -65,20 +98,81 @@ namespace backend2
                 }
 
                 if (args[0] == "--create-with-demo")
+                {
                     DemoInit();
+                }
+
                 if (args[0] == "--drop-database")
-                    Console.WriteLine((new Model1()).Database.Delete() ? "Successfully done" : "Done with error (no database?)");
+                {
+                    Log((new Model1()).Database.Delete()
+                        ? "Successfully done"
+                        : "Done with error (no database?)");
+                }
 
                 if (args[0] == "--create-database")
                 {
-                    DateTime start = DateTime.Now;
-                    Console.WriteLine((new Model1()).Database.CreateIfNotExists()
-                        ? "New database created in (millis) " + (DateTime.Now - start).TotalMilliseconds.ToString()
-                        : "Already exist");
-
+                    CreateDatabase();
                 }
 
+                if (args[0] == "--run-service")
+                {
+                    CreateDatabase();
+                    host = RunService();
+                    if(host!=null)Log("service start, "+host.BaseAddresses[0]);
+                }
             }
+
+            if(host!=null)//Service started. wait until shutdown with ^c maybe.
+                while (true)
+                {
+                    HandleEvents();
+                    //print logs/events/hz
+                }
+
+
+
+
+            HandleEvents();
+        }
+
+        private static void CreateDatabase()
+        {
+            DateTime start = DateTime.Now;
+            Log((new Model1()).Database.CreateIfNotExists()
+                ? "New database created in (millis) " + (DateTime.Now - start).TotalMilliseconds.ToString()
+                : "Database already exist");
+        }
+
+        private static void HandleEvents()
+        {
+            foreach (var a in events.FindAll(a=>!a.Displayed))
+                Console.WriteLine(a.ToString());
+        }
+    }
+
+    public class Event
+    {
+        private bool displayed;
+        private string message;
+        private DateTime timestamp;
+
+        public Event(string msg)
+        {
+            timestamp = DateTime.Now;
+            displayed = false;
+            message = msg;
+        }
+
+        public bool Displayed => displayed;
+
+        /// <summary>
+        /// custom override with self modifying
+        /// </summary>
+        /// <returns>DateTime : Message</returns>
+        public override string ToString()
+        {
+            displayed = true;
+            return timestamp + " : " + message; //NOTTODO: +severity or smthing. Ignoring: possible over-engineering 
         }
     }
 }
